f55d4ce8affa382caf22e713b274b7923d3efc51
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-12-04 18:03:09 +0700

    ok

---
+++ login/login.js
@@ -0,0 +1,41 @@
+// var inputUsername = document.getElementById('username')
+// var inputPassword = document.getElementById('password')
+// var formLogin = document.getElementById('form-login')
+// var checkLogin = false;
+// var CORRECT_USER = 'tunglam99';
+// var CORRECT_PASSWORD = '1';
+//
+// if (formLogin.attachEvent) {
+//     formLogin.attachEvent('submit',onFormSubmit);
+// } else {
+//     formLogin.addEventListener('submit', onFormSubmit);
+// }
+//
+// function onFormSubmit() {
+//     var username = inputUsername.value;
+//     var password = inputPassword.value;
+//     // var redirURL = 'C:\\Users\\tunglam\\Desktop\\mang1\\cuoiky\\home.html';
+//     // alert('thanh cong'+ '<a href="home.html"></a>')
+//     // if (username === CORRECT_USER && password === CORRECT_PASSWORD){
+//     //     console.log(username,password)
+//     //     if (window.confirm('Đăng nhập thành công!')){
+//     //         window.location.assign(redirURL);
+//     //         window.location = redirURL;
+//     //         window.location.href = redirURL;
+//     //         location.href = redirURL;
+//     //     }
+//     // } else {
+//     //     // window.location.href = 'https://stackoverflow.com/questions/5443568/javascript-windows-alert-with-redirect-function';
+//     //     // alert('Đăng nhập thất bại');
+//     // }
+//     if (window.confirm('Đăng nhập thành công!')){
+//         '   <div class="alert alert-success">  '  +
+//         '       <strong>Success!</strong> You should <a href="#" class="alert-link">read this message</a>.  '  +
+//         '    </div>  ' ;
+//         // window.location.assign(redirURL)
+//         // window.location = redirURL;
+//         // window.location.pathname = 'home.html'
+//         // location.href = 'home.html';
+//         // document.location.href = "home.html"
+//     }
+// }
